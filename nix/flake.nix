d657d7b1242eaf5bcd62ad9ae5873a733dcdf53b
Marek Maksimczyk <marek.maksimczyk@mandos.net.pl>
2023-12-22 14:29:59 +0100

    nix: Create nil and nixd dev shells
    Updates dependencies

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,20 +1,32 @@
 {
   description = "Development environment for Nix";
 
-  outputs = { self, nixpkgs }: {
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixd.url = "github:nix-community/nixd";
+  };
 
-    devShells."x86_64-linux".default =
-      let
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in
-      pkgs.mkShell {
+  outputs = { self, nixpkgs, nixd }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShells."x86_64-linux".default = self.devShells."x86_64-linux".nil;
+
+      devShells."x86_64-linux".nil = pkgs.mkShell {
+        packages = with pkgs; [
+          nil
+          nixpkgs-fmt
+        ];
+      };
+
+      devShells."x86_64-linux".nixd = pkgs.mkShell {
         packages = with pkgs; [
           nixd
           nixpkgs-fmt
         ];
       };
 
-    formatter."x86_64-linux" = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
-
-  };
+      formatter."x86_64-linux" = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+    };
 }
